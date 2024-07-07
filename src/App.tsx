68d0efbbacb74f265fd8e9598b30fc67ec3132f2
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-07-08 03:08:01 +0600

    Добавил примерные типы, создал компоненты, layout и формы для отправки и отобразил ее в контейнере NewMeal, сделал спиннеры заранее. Отобразил то что сделал в App форма отправляется корректно

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-const App = () => (
-    <>
-        da
-    </>
-);
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import NewMeal from './containers/NewMeal/NewMeal';
+import Layout from './components/Layout/Layout';
 
-export default App
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<h1>Meals</h1>}/>
+        <Route path="/new-meal" element={<NewMeal/>}/>
+      </Routes>
+    </Layout>
+  );
+};
+
+export default App;
